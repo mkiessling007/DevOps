4e75f83cd399bdb33d47a6005b87758e95b48a8a
michael.kissling.homeATgmail.com <michael.kiessling.homemail.com>
2018-09-19 08:20:35 +0000

    Added some functionality

--- rest_call.py
+++ rest_call.py
@@ -1,26 +1,34 @@
-#!python
+#!/bin/python
 
 # Import the modules required to make API calls
 import requests
 import json
+from requests.auth import HTTPBasicAuth
 
 # Disable warnings
 requests.packages.urllib3.disable_warnings()
 
-def settings(rest_call = '/api/v1/ticket'):
+
+def settings():
  '''Set all variables.
 
 
- Doesn't take any keyword arguments. Set directly within script.
+ Takes no argument.
 
 
  Returns: tuple (APIC-EM, REST-API call, payload, headers)
  '''
- apic_em_ip = 'https://sandboxapicem.cisco.com'
- payload = {'username':'devnetuser','password':'Cisco123!'}
+ #rest_call = '/api/v1/ticket'
+ #apic_em_ip = 'https://sandboxapicem.cisco.com'
+ #payload = {'username':'devnetuser','password':'Cisco123!'}
+ #headers = {'content-type' : 'application/json'}
+ rest_call = '/api/v1/ticket'
+ apic_em_ip = 'https://primeinfrasandbox.cisco.com'
+ payload = {'username':'devnetuser','password':'DevNet123!'}
  headers = {'content-type' : 'application/json'}
 
  return ((apic_em_ip, rest_call, payload, headers))
+
 
 def getToken():
  '''Get APIC-EM authentication token
@@ -36,17 +44,84 @@ def getToken():
  setting = settings()
  # Build the URL
  url = setting[0] + setting[1]
- # Assign the response to a variable and put it into JSON formatting
+ # Call POST and assign the response to a variable and put it into JSON formatting
  response = requests.post(url, data=json.dumps(setting[2]), headers=setting[3], verify=False).json()
+ # Extract the authentication token
+ token = response['response']['serviceTicket']
 
- # Return the response body
+ # Return the authentication token
+ return (token)
+
+
+def callRest(rest_uri, token):
+ '''REST-API GET request
+
+
+ Takes two arguments:
+ rest_uri = eg /network-device
+ token = authentication token to build the header
+
+
+ Returns: dictionary
+ '''
+ # Build the header
+ headers = {'X-AUTH-TOKEN': token}
+ # Get settings for REST-API call
+ setting = settings()
+ # Build the URL
+ url = setting[0] + rest_uri
+ # Call GET and assign the response to a variable
+ response = requests.get(url, headers=headers, verify=False).json()
+
+ # Return the respons as dictionary json
  return (response)
 
+
+def callPrime(rest_uri):
+ '''REST-API GET request
+
+
+ Takes two arguments:
+ rest_uri = eg /network-device
+
+
+ Returns: dictionary
+ '''
+ # Get settings
+ setting = settings()
+ # Build the HTTP authentication
+ basic_auth = HTTPBasicAuth(setting[2]['username'], setting[2]['password'])
+ # Build the URL
+ url = setting[0] + rest_uri
+ # Call GET and assign the response to a variable
+ response = requests.get(url, auth=basic_auth, verify=False).json()
+
+ # Return the respons as dictionary json
+ return (response)
+
+
+def hJson(jsonDict):
+ '''Print json in a human readable format
+ 
+ 
+ Takes one argument:
+ jsonDict = A json formatted data
+ 
+ 
+ Returns: string
+ '''
+ readable = json.dumps(jsonDict, sort_keys=True, indent=4)
+ return (readable)
+ 
+
 def main():
- token = getToken()
- #print (token)
- token = token['response']['serviceTicket']
- print ('Authentication Token: ' + token)
+ #token = getToken()
+ #output = callRest('/api/v1/network-device', token)
+ #for device in output['response']:
+     #print ('Hostname: ' + device['hostname'])
+ output = callPrime('/webacs/api/v3/data/Devices.json')
+ print (hJson(output))
 
 if __name__ == '__main__':
  main()
+
